5596b0666da0e87e60d531e5956500b96a1a96e3
egami jun <egamijun@24.local>
2025-05-26 17:19:00 +0800

    WIP: 保存前の変更

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,28 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class AdminSeeder extends Seeder
 {
-    private $user;
-
-    public function __construct(User $user){
-        $this->user = $user;
-    }
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //Add 1 admin User
-        $this->user->name = 'Administrator';
-        $this->user->email = 'admin@gmail.com';
-        $this->user->password = Hash::make('asdfasdf');
-        $this->user->role_id = user::ADMIN_ROLE_ID;
-        $this->user->save();
+        // admin がすでに存在していればスキップ
+        if (!User::where('email', 'admin@gmail.com')->exists()) {
+            User::create([
+                'name' => 'Administrator',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('asdfasdf'),
+                'role_id' => User::ADMIN_ROLE_ID,
+            ]);
+        }
     }
 }
